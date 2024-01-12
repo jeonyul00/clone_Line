6c03f9f3ff5c03d77e1c0745287824bbc1c8e37f
jeonyul <jeonyul00@gmail.com>
2024-01-12 16:12:16 +0900

    first commit

---
+++ 04_Line/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  04_Line
+//
+//  Created by 전율 on 2024/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 04_Line/_4_LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_LineApp.swift
+//  04_Line
+//
+//  Created by 전율 on 2024/01/12.
+//
+
+import SwiftUI
+
+@main
+struct _4_LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
